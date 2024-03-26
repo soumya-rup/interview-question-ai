2cf292b3e6115e3a1966049c4001498a2e5281a9
Rahul Sailesh Wadhwa <95941770+rahulw311@users.noreply.github.com>
2024-03-26 05:00:12 -0700

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@ sudo apt update
 # Install dependencies (if needed)
 sudo apt install python3-pip  # Assuming you're using Python 3
 # Install libraries using pip
-pip3 install google.generativeai>=1.38 
+pip3 install -q -U google-generativeai 
 pip3 install flask
 pip3 install pandas
 
